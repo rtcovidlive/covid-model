30a55b1326c3d267813afcfb550effdcd0e32fcc
Michael Osthege <michael.osthege@outlook.com>
2020-06-27 18:22:45 +0200

    add basic tests for generative model

--- covid/tests.py
+++ covid/tests.py
@@ -2,6 +2,8 @@ import numpy
 import pandas
 import pytest
 
+import arviz
+
 import covid.data
 import covid.models.generative
 
@@ -15,3 +17,31 @@ class TestDataUS:
         df_raw = covid.data.get_raw_covidtracking_data()
         df_processed = covid.data.process_covidtracking_data(df_raw, pandas.Timestamp('2020-06-25'))
         assert isinstance(df_processed, pandas.DataFrame)
+
+
+class TestGenerative:
+    def test_build(self):
+        df_raw = covid.data.get_raw_covidtracking_data()
+        df_processed = covid.data.process_covidtracking_data(df_raw, pandas.Timestamp('2020-06-25'))
+        model = covid.models.generative.GenerativeModel(
+            region='NY',
+            observed=df_processed.xs('NY')
+        )
+        model.build()
+
+    def test_sample_and_idata(self):
+        df_raw = covid.data.get_raw_covidtracking_data()
+        df_processed = covid.data.process_covidtracking_data(df_raw, pandas.Timestamp('2020-06-25'))
+        model = covid.models.generative.GenerativeModel(
+            region='NY',
+            observed=df_processed.xs('NY')
+        )
+        model.build()
+        model.sample(
+            cores=1, chains=2, tune=50, draws=70
+        )
+        assert model.trace is not None
+        idata = model.inference_data
+        assert isinstance(idata, arviz.InferenceData)
+        assert idata.posterior.attrs["model_version"] == model.version
+        assert "date" in idata.posterior.coords
