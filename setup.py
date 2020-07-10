64f2c17c770bba80b5f0f7e8340cf6c2846229ec
Thomas Wiecki <thomas.wiecki@gmail.com>
2020-07-10 16:17:48 +0200

    Change copyright owner.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#   Copyright 2020 The PyMC Developers
+#   Copyright 2020 Kevin Systrom
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
