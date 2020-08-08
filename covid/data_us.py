c53de8c14c1186b04e2a440b0b40217b1ed568b8
Mike Krieger <m@mikeyk.co>
2020-08-08 22:30:48 +0000

    zero out latest HI

--- covid/data_us.py
+++ covid/data_us.py
@@ -102,6 +102,8 @@ def process_covidtracking_data(data: pd.DataFrame, run_date: pd.Timestamp):
         :,
     ] = 0
 
+    data.loc[idx["HI", pd.Timestamp("2020-08-07")], :] = 0
+
     # Zero out any rows where positive tests equal or exceed total reported tests
     # Do not act on Wyoming as they report positive==total most days
     filtering_date = pd.Timestamp('2020-07-27')
